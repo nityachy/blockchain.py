e0a579e9d0985e0e2d278cbc80e550ed629d151b
nityachy <93797446+nityachy@users.noreply.github.com>
2021-12-26 20:05:11 -0500

    Add files via upload

---
+++ blockchain.py
@@ -0,0 +1,59 @@
+import hashlib as hasher
+
+# each block needs to be stored with timestamp/index
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp 
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.hash_block()
+
+    def hash_block(self):
+        sha = hasher.sha256()
+        sha.update(str(self.index) +
+            str(self.timestamp) +
+            str(self.data) +
+            str(self.previous_hash))
+
+        return sha.hexdigest()
+
+#block structure done
+
+
+import datetime as date 
+
+def create_genesis_block():
+    return Block(0, date.datetime.now(), "Genesis Block", "0")
+
+#genesis block done - block of index 0 
+#manually constructed in many cases
+
+def next_block(last_block):
+    this_index = last_block.index + 1
+    this_timestamp = date.datetime.now()
+    this_data = "This is a Block" + str(this_index)
+    this_hash = last_block.hash
+    return Block(this_index, this_timestamp, this_data, this_hash)    
+
+#function will take previous block in chain as parameter
+
+#loop + add 30 blocks
+
+blockchain = [create_genesis_block()]
+previous_block = blockchain[0]
+
+#30 blocks added after genesis block
+
+num_of_blocks_to_add = 30
+
+for i in range(0, num_of_blocks_to_add):
+    block_to_add = next_block(previous_block)
+    blockchain.append(block_to_add)
+    previous_block = block_to_add
+
+    print "Block #{} has been added to this blockchain".format(block_to_add.index)
+    print "Hash: {}\n".format(block_to_add.hash)
+
+#error with print statements 
